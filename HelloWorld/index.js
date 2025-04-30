66d7075c81b9aaa9d74c3afcf8e7ab1d73eec797
RawailAhmed12345 <bscs2312170@szabist.pk>
2025-04-30 13:18:00 +0000

    Create get Methods

---
+++ HelloWorld/index.js
@@ -0,0 +1,40 @@
+const express=require('express');
+const cors=require('cors');
+const pool=require('./db');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/region',async(req,res)=>{
+    try{
+        const result = await pool.query('select * from regions');
+        res.json(result.rows);
+    }catch(err) {
+        res.status(500).json({Error:err.message})
+    }
+});
+
+
+app.get('/employees',async(req,res)=>{
+    try{
+        const result = await pool.query('SELECT COUNT(employee_id) AS total_employees FROM employees');
+        res.json(result.rows);
+    }catch(err) {
+        res.status(500).json({Error:err.message})
+    }
+});
+
+app.get('/',async(req,res)=>{
+    try{
+        res.json('Welcome to Hello World')
+    }catch(err) {
+        res.status(500).json({Error:err.message})
+    }
+});
+
+const PORT = process.env.PORT || 6005;
+app.listen(PORT,()=>{
+    console.log(`Connected Successfully...on PORT ${PORT}`)
+});
